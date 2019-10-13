ad427f37c22b6e43f75bbf8b23de9b5ca9a87779
KornelGM <KORNik_k@wp.pl>
2019-10-13 18:16:39 +0200

    Added rudimentary debug functionality (full pause and restart)

---
+++ Assets/Scripts/CheatAndDebug.cs
@@ -0,0 +1,25 @@
+﻿/**
+ * Description: Cheat and Debug functionality.
+ * Authors: Kornel
+ * Copyright: © 2019 Kornel. All rights reserved. For license see: 'LICENSE.txt'
+ **/
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheatAndDebug : MonoBehaviour
+{
+	void Update ()
+	{
+		if ( Input.GetKeyDown( KeyCode.R ) )
+			SceneManager.LoadScene( 0 );
+
+		if ( Input.GetKeyDown( KeyCode.P ) )
+		{
+			if ( Time.timeScale == 0f )
+				Time.timeScale = 1f;
+			else
+				Time.timeScale = 0f;
+		}
+	}
+}
