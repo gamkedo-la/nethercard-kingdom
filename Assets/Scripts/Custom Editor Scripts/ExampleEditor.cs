896aad078dbd4200eae4ead58027c219883c1aea
Brian Nielsen <47045697+brnielsen@users.noreply.github.com>
2019-10-14 16:28:38 -0400

    Card Creator Custom Editor WIP

---
+++ Assets/Scripts/Custom Editor Scripts/ExampleEditor.cs
@@ -0,0 +1,38 @@
+﻿//Thank you Brackeys :)
+using UnityEngine;
+using UnityEditor;
+
+public class ExampleEditor : EditorWindow
+{
+
+    Color color;
+
+    [MenuItem("Window/Card Creator (WIP)")]
+    public static void ShowWindow()
+    {
+        GetWindow<ExampleEditor>("Card Creator");
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Create card", EditorStyles.boldLabel);
+
+        
+
+        if(GUILayout.Button("Finish Card"))
+        {
+            CreateCardVariant();
+        }
+
+    }  
+
+    void CreateCardVariant()
+    {
+        string prefabPath = "Prefabs/Card Template";
+        string localPath = "Assets/Resources/Prefabs/Player Cards/New Card.prefab";
+        Object source = Resources.Load(prefabPath);
+        GameObject objSource = (GameObject)PrefabUtility.InstantiatePrefab(source);
+        GameObject obj = PrefabUtility.SaveAsPrefabAsset(objSource, localPath);
+        obj.name = "New Card";
+    }
+}
