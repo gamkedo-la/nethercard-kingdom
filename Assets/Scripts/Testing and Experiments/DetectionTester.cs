698dfdf42adbdfc7915ee5246b31150f056a43e0
KornelGM <KORNik_k@wp.pl>
2019-10-17 12:18:50 +0200

    Added HomeTeam logo and detection test script

---
+++ Assets/Scripts/Testing and Experiments/DetectionTester.cs
@@ -0,0 +1,42 @@
+﻿/**
+ * Description: Tests mouse detection.
+ * Authors: Kornel
+ * Copyright: © 2019 Kornel. All rights reserved. For license see: 'LICENSE.txt'
+ **/
+
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class DetectionTester : MonoBehaviour
+{
+	void Start ()
+	{
+		//Assert.IsNotNull( , $"Please assign <b>{nameof(  )}</b> field on <b>{GetType( ).Name}</b> script on <b>{name}</b> object" );
+
+	}
+
+	void Update ()
+	{
+
+	}
+
+	public void OnEnter()
+	{
+		Debug.Log( "On" );
+	}
+
+	public void OnExit( )
+	{
+		Debug.Log( "Off" );
+	}
+
+	private void OnMouseEnter( )
+	{
+		Debug.Log( "M On" );
+	}
+
+	private void OnMouseExit( )
+	{
+		Debug.Log( "M Off" );
+	}
+}
