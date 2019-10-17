c7aa495125921e4ebc2c61d5ce361649f40db6ec
Brian Nielsen <47045697+brnielsen@users.noreply.github.com>
2019-10-17 11:55:35 -0400

    Card Editor.  User can pass some card data to new card prefab variant

--- Assets/Scripts/Custom Editor Scripts/ExampleEditor.cs
+++ Assets/Scripts/Custom Editor Scripts/ExampleEditor.cs
@@ -4,7 +4,16 @@ using UnityEditor;
 public class ExampleEditor : EditorWindow
 {
     string cardName = "New Card Name";
-    Color color;
+    CardType cardType;
+    private Card cardData;
+    private int cardCost = 0;
+    private int attack = 0;
+    private int hp = 0;
+    private float speed = 0;
+    private string abilityText = "Ability";
+    private string flavorText = "Lore?";
+
+    private Sprite cardArt = null;
 
     [MenuItem("Window/Card Creator (WIP)")]
     public static void ShowWindow()
@@ -14,10 +23,28 @@ public class ExampleEditor : EditorWindow
 
     void OnGUI()
     {
-
+        
         GUILayout.Label("Create card", EditorStyles.boldLabel);
 
         cardName = EditorGUILayout.TextField("Card Name", cardName);
+        cardType = (CardType)EditorGUILayout.EnumFlagsField("Card Type", cardType);        
+        cardCost = EditorGUILayout.IntField("Card Cost", cardCost);
+        attack = EditorGUILayout.IntField("Attak Power", attack);
+        hp = EditorGUILayout.IntField("Hit Points", hp);
+        speed = EditorGUILayout.FloatField("Speed", speed);
+        abilityText = EditorGUILayout.TextField("Ability Text", abilityText);
+        flavorText = EditorGUILayout.TextField("Flavor Text", flavorText);
+
+        
+
+        /* [SerializeField] private CardType type = CardType.Unit;
+      [SerializeField] private int useCost = 2;
+      [SerializeField] private int attack = 1;
+      [SerializeField] private int hp = 10;
+      [SerializeField] private float speed = 2.5f;
+      [SerializeField] private string displayName = "Unnamed Card";
+      [SerializeField] private string abilityText = "This is just a test description...";
+      [SerializeField] private string flavorText = "What a lovely card!";*/
 
         if (GUILayout.Button("Finish Card"))
         {
@@ -28,11 +55,15 @@ public class ExampleEditor : EditorWindow
 
     void CreateCardVariant()
     {
+        //TODO: Add some error checking.  Currently, if a new card has the same name as an existing card the existing card's data is overwritten
         string prefabPath = "Assets/Prefabs/Card Template.prefab";
         string localPath = "Assets/Prefabs/Player Cards/" + cardName + ".prefab";
         Object cardPrefab = (GameObject)AssetDatabase.LoadAssetAtPath(prefabPath, typeof(GameObject));
         GameObject card = PrefabUtility.InstantiatePrefab(cardPrefab) as GameObject;
         GameObject newCard = PrefabUtility.SaveAsPrefabAsset(card, localPath);
+
+        cardData = newCard.GetComponent<Card>();      
+        cardData.UpdateCardStatsFromEditor(cardType, cardName, cardCost, abilityText, flavorText);
         DestroyImmediate(card);
 
     }
