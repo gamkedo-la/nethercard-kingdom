0ef49aaf53a3bb7616dcf253852c79bdce7a2d10
Christer Kaitila <breakdance@mcfunkypants.com>
2019-10-19 15:34:00 -0700

    nature decorations prefab with a Spin2D script for the sun

---
+++ Assets/Scripts/FX/Spin2D.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin2D : MonoBehaviour
+{
+    public float rotateSpeed;
+
+    void Update () {
+        transform.Rotate(Vector3.forward, rotateSpeed * Time.deltaTime, Space.World);
+    }
+}
