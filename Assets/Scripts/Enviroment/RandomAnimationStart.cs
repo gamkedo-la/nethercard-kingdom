97b58315c7898f36da4aa07dcf65515fa136614f
KornelGM <KORNik_k@wp.pl>
2019-10-21 21:54:28 +0200

    Added a bit of randomness to environment animation start time, forced changes to QualitySettings (perhaps it will stop it from constantly changing on it's own)

---
+++ Assets/Scripts/Enviroment/RandomAnimationStart.cs
@@ -0,0 +1,25 @@
+﻿/**
+ * Description: Starts animation with a delay.
+ * Authors: Kornel
+ * Copyright: © 2019 Kornel. All rights reserved. For license see: 'LICENSE.txt'
+ **/
+
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class RandomAnimationStart : MonoBehaviour
+{
+	private Animator animator = null;
+
+	void Start( )
+	{
+		animator = GetComponent<Animator>( );
+		Assert.IsNotNull( animator, $"Please assign <b>{nameof( animator )}</b> field on <b>{GetType( ).Name}</b> script on <b>{name}</b> object" );
+	}
+
+	void OnEnable( )
+	{
+		animator = GetComponent<Animator>( );
+		animator.Play( 0, 0, Random.Range( 0f, 0.3f ) );
+	}
+}
