758d3f050d48c2e325c1f3646abbd99738d4840e
KornelGM <KORNik_k@wp.pl>
2019-10-23 17:43:50 +0200

    Sprites on the field should now display in correct sorting order

---
+++ Assets/Scripts/Cards/AutomaticSpriteOrder.cs
@@ -0,0 +1,26 @@
+﻿/**
+ * Description: Sets sprite order based on Y position.
+ * Authors: Kornel
+ * Copyright: © 2019 Kornel. All rights reserved. For license see: 'LICENSE.txt'
+ **/
+
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Rendering;
+
+[RequireComponent(typeof(SortingGroup))]
+public class AutomaticSpriteOrder : MonoBehaviour
+{
+	private SortingGroup sorting = null;
+
+	void Start ()
+	{
+		sorting = GetComponent<SortingGroup>( );
+		Assert.IsNotNull( sorting, $"Please assign <b>{nameof( sorting )}</b> field on <b>{GetType( ).Name}</b> script on <b>{name}</b> object" );
+	}
+
+	void FixedUpdate ()
+	{
+		sorting.sortingOrder = -(int)(transform.position.y * 100);
+	}
+}
