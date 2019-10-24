c27e20e9e23df0edf5b6bee099fd349720371810
Vaan Hope Khani <e.t.khani@gmail.com>
2019-10-24 03:37:07 -0800

    Added buttonprefabs to pausedMenuUI

--- Assets/Scripts/UI/PausedMenu.cs
+++ Assets/Scripts/UI/PausedMenu.cs
@@ -10,7 +10,7 @@ public class PausedMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        pauseMenuUI.SetActive(false);
     }
 
     // Update is called once per frame
