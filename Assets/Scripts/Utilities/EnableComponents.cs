5c7d81061efcea0259e11d4e01cbf711ef69ddb5
KornelGM <KORNik_k@wp.pl>
2019-10-25 12:28:46 +0200

    New unit summon animation

---
+++ Assets/Scripts/Utilities/EnableComponents.cs
@@ -0,0 +1,25 @@
+﻿/**
+ * Description: Allows to enable components.
+ * Authors: Kornel
+ * Copyright: © 2019 Kornel. All rights reserved. For license see: 'LICENSE.txt'
+ **/
+
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class EnableComponents : MonoBehaviour
+{
+	[SerializeField] private Behaviour[] toEnable = null;
+
+	void Start( )
+	{
+		Assert.AreNotEqual( toEnable.Length, 0, $"Please assign <b>{nameof( toEnable )}</b> field on <b>{GetType( ).Name}</b> script on <b>{name}</b> object" );
+
+	}
+
+	public void DoEnabling( )
+	{
+		foreach ( var component in toEnable )
+			component.enabled = true;
+	}
+}
