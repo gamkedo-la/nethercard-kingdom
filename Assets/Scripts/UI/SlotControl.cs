14c6fef0f7cbd089977ffac52460c5e283124457
KornelGM <KORNik_k@wp.pl>
2019-10-27 12:34:12 +0100

    Moved files in to corresponding folders, fixed warnings

--- Assets/Scripts/UI/SlotControl.cs
+++ Assets/Scripts/UI/SlotControl.cs
@@ -1,22 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class SlotControl : MonoBehaviour
 {
-    [SerializeField] private Image emptySlotFill;
-    [SerializeField] private Color hoverColor;
+    [SerializeField] private Image emptySlotFill = null;
+    [SerializeField] private Color hoverColor = Color.white;
 
     private Color currentFillColor = Color.white;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
         emptySlotFill.color = Color.Lerp(emptySlotFill.color, currentFillColor, 0.25f);
