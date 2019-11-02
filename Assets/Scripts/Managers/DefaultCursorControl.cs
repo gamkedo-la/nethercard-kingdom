c581595bae97456382aac7fef41e05df66d89f71
A. Joël Lamotte (Klaim) <mjklaim@gmail.com>
2019-11-02 14:56:53 +0100

    Adds behaviors to control the mouse cursor (buggy - wip)

---
+++ Assets/Scripts/Managers/DefaultCursorControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class DefaultCursorControl : MonoBehaviour
+{
+    public Texture2D cursorTexture = null;
+    public Vector2 hotSpot = Vector2.zero;
+
+    private static DefaultCursorControl current = null;
+
+    void Start()
+    {
+        current = this;
+        reset();
+    }
+
+    public static void resetCursor()
+    {
+        current?.reset();
+    }
+
+    public void reset()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, CursorMode.Auto);
+    }
+
+}
---
+++ Assets/Scripts/UI/CursorControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CursorControl : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Texture2D cursorTexture = null;
+    public Vector2 hotSpot = Vector2.zero;
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        //Debug.Log("POINTER ENTER");
+        Cursor.SetCursor(cursorTexture, hotSpot, CursorMode.Auto);
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        // Debug.Log("POINTER EXIT");
+        //Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        DefaultCursorControl.resetCursor();
+    }
+
+}
