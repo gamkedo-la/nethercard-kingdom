b774af08bd1dacd74b664fc789cf24c04d39ab38
KornelGM <KORNik_k@wp.pl>
2019-11-08 17:39:43 +0100

    Added more options and tooltips to cheat/debug manager

--- Assets/Scripts/Managers/CheatAndDebug.cs
+++ Assets/Scripts/Managers/CheatAndDebug.cs
@@ -12,9 +12,17 @@ public class CheatAndDebug : MonoBehaviour
 	public static CheatAndDebug Instance { get; private set; }
 	public bool ShowDebugInfo { get { return showDebugInfo; } }
 	public bool AllowCheats { get { return allowCheats; } }
+	public bool UseAlternate { get { return useAlternate; } }
 
+	[Header("Debug Flags")]
+	[Tooltip("Show debug informations")]
 	[SerializeField] private bool showDebugInfo = false;
-	[SerializeField] private bool allowCheats = true;
+	[Tooltip("Allow cheat codes")]
+	[SerializeField] private bool allowCheats = false;
+	[Tooltip("Use alternative, parallel feature implementation")]
+	[SerializeField] private bool useAlternate = false;
+
+	[Header("Debug Parameters")]
 	[SerializeField] private int manaCheatAmount = 20;
 
 	private void Awake( )
