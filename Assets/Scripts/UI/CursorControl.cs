78200dfe012590407c391ad5da71f2ead0a3bdf2
A. Joël Lamotte (Klaim) <mjklaim@gmail.com>
2019-11-10 00:37:46 +0100

    Improved cursor images behavior, wip.

--- Assets/Scripts/UI/CursorControl.cs
+++ Assets/Scripts/UI/CursorControl.cs
@@ -1,21 +1,46 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class CursorControl : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+[System.Serializable]
+public struct CursorConfig 
 {
-    public Texture2D cursorTexture = null;
-    public Vector2 hotSpot = Vector2.zero;
+    public Texture2D texture;
+    public Vector2 hotSpot;
+}
 
+public class CursorControl : MonoBehaviour, 
+    IPointerEnterHandler, IPointerExitHandler
+{
+    
+    public CursorConfig onMouseOver;
+    public CursorConfig onMouseDragging;
+    
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
-        //Debug.Log("POINTER ENTER");
-        Cursor.SetCursor(cursorTexture, hotSpot, CursorMode.Auto);
+        Debug.Log("POINTER ENTER ", this);
+        if (!pointerEventData.dragging)
+        {
+            Cursor.SetCursor(onMouseOver.texture, onMouseOver.hotSpot, CursorMode.Auto);
+        }
     }
-
     public void OnPointerExit(PointerEventData pointerEventData)
     {
-        // Debug.Log("POINTER EXIT");
-        //Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
-        DefaultCursorControl.ResetCursor();
+        Debug.Log("POINTER EXIT ", this);
+        if (!pointerEventData.dragging)
+        {
+            DefaultCursorControl.ResetCursor();
+        }
     }
+    
+    public void OnDraggingBegin()
+    {
+        Debug.Log("DRAGGING BEGIN ", this);
+        Cursor.SetCursor(onMouseDragging.texture, onMouseDragging.hotSpot, CursorMode.Auto);
+    }
+    public void OnDraggingEnd()
+    {
+        Debug.Log("DRAGGING END ", this);
+        Cursor.SetCursor(onMouseOver.texture, onMouseOver.hotSpot, CursorMode.Auto);
+    }
+
 }
