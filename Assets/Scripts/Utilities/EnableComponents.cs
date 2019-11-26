53f5866160969dfd4f5aa3426506ffd2837467d4
KornelGM <KORNik_k@wp.pl>
2019-11-26 13:57:24 +0100

    Added "Prepare your deck" message first time the deck builder is shown

--- Assets/Scripts/Utilities/EnableComponents.cs
+++ Assets/Scripts/Utilities/EnableComponents.cs
@@ -10,10 +10,14 @@ using UnityEngine.Assertions;
 public class EnableComponents : MonoBehaviour
 {
 	[SerializeField] private Behaviour[] toEnable = null;
+	[SerializeField] private float autoEnableTime = 0;
 
 	void Start( )
 	{
 		Assert.AreNotEqual( toEnable.Length, 0, $"Please assign <b>{nameof( toEnable )}</b> field on <b>{GetType( ).Name}</b> script on <b>{name}</b> object" );
+
+		if ( autoEnableTime > 0 )
+			Invoke( nameof( DoEnabling ), autoEnableTime );
 	}
 
 	public void DoEnabling( )
