aebe03e7b45f34ca6814b1b77a25b962dfcfbd6c
Brian Nielsen <47045697+brnielsen@users.noreply.github.com>
2019-11-27 13:01:24 -0500

    Added simple Projectile script

---
+++ Assets/Scripts/Units/Projectile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    private float projectileSpeed = 1.5f;
+    [SerializeField]
+    private float attackRate = 1.0f;
+    [SerializeField]
+    private float initialAttackDelay = 0.5f;
+    [SerializeField]
+    private GameObject projectilePrefab = null;
+    [SerializeField]
+    private Transform attackOrigin;
+
+
+    void Awake()
+    {
+        InvokeRepeating("Attack", initialAttackDelay, attackRate);
+    }
+
+
+    private void Attack()
+    {
+        GameObject attackProjectile = Instantiate(projectilePrefab, attackOrigin);            
+
+    }
+
+   
+
+}
