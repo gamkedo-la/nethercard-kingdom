939f40c1a1084e82bb50daa597755c2eccccc06a
KornelGM <KORNik_k@wp.pl>
2019-11-28 09:37:11 +0100

    Game speed functionality moved to level manager, is now set on scene load, allows for a range from 1/4 to 4x and is set in desecrate steps

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -4,12 +4,14 @@
  * Copyright: © 2019 Kornel. All rights reserved. For license see: 'LICENSE.txt'
  **/
 
+using TMPro;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.Events;
 
 public class LevelManager : MonoBehaviour
 {
+	[SerializeField] private TextMeshProUGUI gameSpeedLabel = null;
 	[SerializeField] private UnitsManager unitsManager = null;
 	[SerializeField] private GameObject wonScreen = null;
 	[SerializeField] private GameObject gameoverScreen = null;
@@ -20,11 +22,60 @@ public class LevelManager : MonoBehaviour
 
 	void Start ()
 	{
+		Assert.IsNotNull( gameSpeedLabel, $"Please assign <b>{nameof( gameSpeedLabel )}</b> field on <b>{GetType( ).Name}</b> script on <b>{name}</b> object" );
 		Assert.IsNotNull( unitsManager, $"Please assign <b>{nameof( unitsManager )}</b> field on <b>{GetType( ).Name}</b> script on <b>{name}</b> object" );
 		Assert.IsNotNull( wonScreen, $"Please assign <b>{nameof( wonScreen )}</b> field on <b>{GetType( ).Name}</b> script on <b>{name}</b> object" );
 		Assert.IsNotNull( gameoverScreen, $"Please assign <b>{nameof( gameoverScreen )}</b> field on <b>{GetType( ).Name}</b> script on <b>{name}</b> object" );
 		Assert.IsNotNull( wonSound, $"Please assign <b>{nameof( wonSound )}</b> field on <b>{GetType( ).Name}</b> script on <b>{name}</b> object" );
 		Assert.IsNotNull( gameoverSound, $"Please assign <b>{nameof( gameoverSound )}</b> field on <b>{GetType( ).Name}</b> script on <b>{name}</b> object" );
+
+		SetGameSpeed( 3.0f ); // Normal speed
+	}
+
+	public void SetGameSpeed( float value )
+	{
+		float speed = 0;
+		string label = "";
+
+		switch ( (int)value )
+		{
+			case 0:
+			speed = 0.0f;
+			label = "PAUSED";
+			break;
+
+			case 1:
+			speed = 0.25f;
+			label = "0.25x Speed";
+			break;
+
+			case 2:
+			speed = 0.05f;
+			label = "0.5x Speed";
+			break;
+
+			case 3:
+			speed = 1.0f;
+			label = "Normal Speed";
+			break;
+
+			case 4:
+			speed = 2.0f;
+			label = "2x Speed";
+			break;
+
+			case 5:
+			speed = 4.0f;
+			label = "4x Speed";
+			break;
+
+			default:
+			break;
+		}
+
+		Time.timeScale = speed;
+		Time.fixedDeltaTime = Time.timeScale * 0.02f;
+		gameSpeedLabel.text = label;
 	}
 
 	public void PlayerKilled( )
