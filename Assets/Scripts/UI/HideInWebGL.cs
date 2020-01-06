ed6f261a6ff7378699b4dd000d3fdcd248b4324f
KornelGM <KORNik_k@wp.pl>
2020-01-06 17:39:45 +0100

    Quite button will be hidden in WebGL build

---
+++ Assets/Scripts/UI/HideInWebGL.cs
@@ -0,0 +1,17 @@
+﻿/**
+ * Description: Hides object in WebGL.
+ * Authors: Kornel
+ * Copyright: © 2019 Kornel. All rights reserved. For license see: 'LICENSE.txt'
+ **/
+
+using UnityEngine;
+
+public class HideInWebGL : MonoBehaviour
+{
+	void Start ()
+	{
+		#if UNITY_WEBGL
+		gameObject.SetActive( false );
+		#endif
+	}
+}
